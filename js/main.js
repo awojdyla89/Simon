0efa8e51ba82bdbbf0df245ab3e7f66dcb715947
Adam Wojdyla <adam@Adams-MacBook-Pro.local>
2021-05-29 15:35:49 -0600

    enhanced mouse functionality by reducing code redundancy

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
 /*----- constants -----*/
-// create array to store random generated sequence
-// create a user sequence array to store buttons clicked
 
-const userSequence = [];
-const computerSequence = [];
-
-// each button when clicked needs to light up and turn off when released 
-// create a function for each color: 
-// when clicked change the color of button with DOM
-// when released change the color back with DOM
 
 /*----- app's state (variables) -----*/
 let playerName;
 let highScore;
 let currentScore;
+let userSequence = [];
+let computerSequence = [];
 
 /*----- cached element references -----*/
 
  playerName = document.querySelector('#player-name');
 
 let cScore = document.querySelector('#current-result');
-//cScore.innerText = 2;
 let hScore = document.querySelector('#high-result');
-//hScore.innerText = 27;
 let greenBtn = document.querySelector('.green-button');
 let yellowBtn = document.querySelector('.yellow-button');
 let redBtn = document.querySelector('.red-button');
@@ -33,50 +24,50 @@ let resetBtn = document.querySelector('#reset-button');
 
 /*----- event listeners -----*/
 
-document.querySelector('#start-button').addEventListener('mousedown', playGame);
-document.querySelector('#start-button').addEventListener('mouseup', playGame);
-document.querySelector('#reset-button').addEventListener('mousedown', resetGame);
-document.querySelector('#reset-button').addEventListener('mouseup', resetGame);
+document.querySelector('#start-button').addEventListener('click', getPlayerName);
+document.querySelector('#reset-button').addEventListener('click', init);
+document.querySelector('.quarter-panel').addEventListener('click', flash);
 
+function flash(){
+document.querySelectorAll('.quarter-panel').forEach(item =>{
+    item.addEventListener('mousedown', flashButton => {
+        item.style.backgroundColor = 'white';
+    })
+    document.querySelectorAll('.quarter-panel').forEach(newItem =>{
+        newItem.addEventListener('mouseup', resetButton =>{
+            newItem.style.backgroundColor = item.style.backgroundColor;
+        })
+    })
+})
+}
 
-
-document.querySelector('.green-button').addEventListener('mousedown', changeToWhite);
-document.querySelector('.green-button').addEventListener('mouseup', clearColor);
-document.querySelector('.yellow-button').addEventListener('mousedown', changeToWhite);
-document.querySelector('.yellow-button').addEventListener('mouseup', clearColor);
-document.querySelector('.red-button').addEventListener('mousedown', changeToWhite);
-document.querySelector('.red-button').addEventListener('mouseup', clearColor);
-document.querySelector('.blue-button').addEventListener('mousedown', changeToWhite);
-document.querySelector('.blue-button').addEventListener('mouseup', clearColor);
-
-
-
+init();
 /*----- functions -----*/
 
 // create an init() function that creates the starting foundation of the game
-
-// create a render() function that updates: 
-// - the player name
-// - the current score
-// - the high score
-// - plays the updated random computer sequence for the buttons
+function init(){
+    playerName.innerText = '';
+    cScore.innerText = 0;
+    hScore.innerText = 0;
+    userSequence = [];
+    computerSequence = [];
+    
+    render();
+}
 
 function render(){
-    //clearColor();
 }
 
 
 // create a randomNum function that generates a random number between 1-4
-function randomNum(){
+function compGenSequence(){
+    let choices = ['red', 'yellow', 'blue', 'red'];
     let randomIndex =  Math.floor(Math.random() * 4) + 1;
-    return randomIndex;
+    computerSequence.push(choices[randomIndex]);
+    return computerSequence;
 }
 
-// create a function that will run the computer generated sequence at a specfic time interval 
 
-// create a function that increments the random sequence using the randomNum helper function
-
-// basic checks of DOM manipulation for buttons
 
 //function to get player name
 function getPlayerName(){
@@ -84,13 +75,18 @@ function getPlayerName(){
     if (namePrompt != null){
         playerName.innerText = namePrompt;
     }
+    return playerName;
 }
 
-//getPlayerName();
 
+
+function resetGame(){
+    console.log("reset button clicked");
+}
 
 function changeToWhite(e){
 e.target.style.backgroundColor = 'white';
+
     
 }
 function clearColor(){
@@ -104,11 +100,3 @@ function clearColor(){
 
 
 
-function playGame(e){
-    changeToWhite(e);
-    clearColor();
-    console.log("start button clicked");
-}
-function resetGame(){
-    console.log("reset button clicked");
-}
