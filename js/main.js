dd83441b47fd7beff319fc90a72670347d884d6e
Adam Wojdyla <adam@Adams-MacBook-Pro.local>
2021-05-29 13:29:12 -0600

    added mouse event functionality to gameboard

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 // create array to store random generated sequence
 // create a user sequence array to store buttons clicked
 
+const userSequence = [];
+const computerSequence = [];
+
 // each button when clicked needs to light up and turn off when released 
 // create a function for each color: 
 // when clicked change the color of button with DOM
@@ -14,16 +17,37 @@ let currentScore;
 
 /*----- cached element references -----*/
 
+ playerName = document.querySelector('#player-name');
+
+let cScore = document.querySelector('#current-result');
+//cScore.innerText = 2;
+let hScore = document.querySelector('#high-result');
+//hScore.innerText = 27;
+let greenBtn = document.querySelector('.green-button');
+let yellowBtn = document.querySelector('.yellow-button');
+let redBtn = document.querySelector('.red-button');
+let blueBtn = document.querySelector('.blue-button');
+let startBtn = document.querySelector('#start-button');
+let resetBtn = document.querySelector('#reset-button');
+
+
 /*----- event listeners -----*/
 
-// get user text input for name <input type='text'/>
-// get 'start' button click event
-// get 'reset' button click event
-// get 'audio theme' button click event
-// get 'neon blue' button click event
-//get 'neon red' button click event
-//get 'neon green' button click event
-//get 'neon yellow' button click event
+document.querySelector('#start-button').addEventListener('mousedown', playGame);
+document.querySelector('#start-button').addEventListener('mouseup', playGame);
+document.querySelector('#reset-button').addEventListener('mousedown', resetGame);
+document.querySelector('#reset-button').addEventListener('mouseup', resetGame);
+
+
+
+document.querySelector('.green-button').addEventListener('mousedown', changeToWhite);
+document.querySelector('.green-button').addEventListener('mouseup', clearColor);
+document.querySelector('.yellow-button').addEventListener('mousedown', changeToWhite);
+document.querySelector('.yellow-button').addEventListener('mouseup', clearColor);
+document.querySelector('.red-button').addEventListener('mousedown', changeToWhite);
+document.querySelector('.red-button').addEventListener('mouseup', clearColor);
+document.querySelector('.blue-button').addEventListener('mousedown', changeToWhite);
+document.querySelector('.blue-button').addEventListener('mouseup', clearColor);
 
 
 
@@ -36,6 +60,12 @@ let currentScore;
 // - the current score
 // - the high score
 // - plays the updated random computer sequence for the buttons
+
+function render(){
+    //clearColor();
+}
+
+
 // create a randomNum function that generates a random number between 1-4
 function randomNum(){
     let randomIndex =  Math.floor(Math.random() * 4) + 1;
@@ -47,35 +77,38 @@ function randomNum(){
 // create a function that increments the random sequence using the randomNum helper function
 
 // basic checks of DOM manipulation for buttons
-let pName = document.querySelector('#player-name');
-pName.innerText = "Adam"
-let cScore = document.querySelector('#current-result');
-cScore.innerText = 2;
-let hScore = document.querySelector('#high-result');
-hScore.innerText = 27;
 
-function playTime(){
+//function to get player name
+function getPlayerName(){
+    let namePrompt = prompt("Please enter your name");
+    if (namePrompt != null){
+        playerName.innerText = namePrompt;
+    }
+}
+
+//getPlayerName();
+
+
+function changeToWhite(e){
+e.target.style.backgroundColor = 'white';
+    
+}
+function clearColor(){
+    greenBtn.style.backgroundColor = 'green';
+    redBtn.style.backgroundColor = 'red';
+    yellowBtn.style.backgroundColor = 'yellow';
+    blueBtn.style.backgroundColor = 'blue';
+    startBtn.style.backgroundColor = 'rgb(48, 45, 45)';
+    resetBtn.style.backgroundColor = 'rgb(48, 45, 45)';
+}
+
+
+
+function playGame(e){
+    changeToWhite(e);
+    clearColor();
     console.log("start button clicked");
 }
 function resetGame(){
     console.log("reset button clicked");
 }
-function greenClick(){
-    console.log("green button clicked");
-}
-function yellowClick(){
-    console.log("yellow button clicked");
-}
-function redClick(){
-    console.log("red button clicked");
-}
-function blueClick(){
-    console.log("blue button clicked");
-}
-
- document.querySelector('#start-button').addEventListener('click', playTime);
- document.querySelector('#reset-button').addEventListener('click', resetGame);
- document.querySelector('.green-button').addEventListener('click', greenClick);
- document.querySelector('.yellow-button').addEventListener('click', yellowClick);
- document.querySelector('.red-button').addEventListener('click', redClick);
- document.querySelector('.blue-button').addEventListener('click', blueClick);
