ecd390daa2fb94fd61fc2b1daa5e9ff65cb9d8c3
Adam Wojdyla <adam@Adams-MacBook-Pro.local>
2021-05-29 21:56:32 -0600

    experimented with setInterval() and setTimout() events

--- js/main.js
+++ js/main.js
@@ -24,11 +24,19 @@ let resetBtn = document.querySelector("#reset-button");
 
 //document.querySelector("#start-button").addEventListener("click", render);
 document.querySelector("#reset-button").addEventListener("click", init);
-document.querySelector(".quarter-panel").addEventListener("click", flash);
-greenBtn.addEventListener("click", userGenSequence);
-redBtn.addEventListener("click", userGenSequence);
-yellowBtn.addEventListener("click", userGenSequence);
-blueBtn.addEventListener("click", userGenSequence);
+//greenBtn.addEventListener("click", blink);
+//redBtn.addEventListener("click", userGenSequence);
+//yellowBtn.addEventListener("click", userGenSequence);
+//blueBtn.addEventListener("click", userGenSequence);
+
+function blink() {
+  greenBtn.style.backgroundColor = "white";
+  setTimeout("setToGreen()", 300);
+  
+}
+function setToGreen() {
+  greenBtn.style.backgroundColor = "green";
+}
 
 init();
 /*----- functions -----*/
@@ -49,34 +57,35 @@ function init() {
 
 // }
 
-
 // create a randomNum function that generates a random number between 1-4
 function compGenSequence() {
-    let choices = ["red", "yellow", "blue", "green"];
-    let randomIndex = Math.floor(Math.random() * choices.length - 1) + 1;
-    computerSequence.push(choices[randomIndex]);
-    return computerSequence;
+  let choices = ["red", "yellow", "blue", "green"];
+  let randomIndex = Math.floor(Math.random() * choices.length - 1) + 1;
+  computerSequence.push(choices[randomIndex]);
+
+  return computerSequence;
 }
 
-function userGenSequence(e) {
-    userSequence.push(e.target.id);
+// function userGenSequence(e) {
+//     userSequence.push(e.target.id);
+//     compGenSequence();
+//     //console.log("user>" + userSequence , "computer> " + computerSequence);
+//     //console.log("user>" + userSequence.length , "computer> " + computerSequence.length);
+//   return userSequence;
+// }
+
+document.querySelectorAll(".quarter-panel").forEach((item) => {
+  item.addEventListener("mousedown", (flashButton) => {
+    userSequence.push(item.id);
     compGenSequence();
-    //console.log("user>" + userSequence , "computer> " + computerSequence)
-  return userSequence;
-}
-
-function flash() {
-  document.querySelectorAll(".quarter-panel").forEach((item) => {
-    item.addEventListener("mousedown", (flashButton) => {
-      item.style.backgroundColor = "white";
-    });
-    document.querySelectorAll(".quarter-panel").forEach((newItem) => {
-      newItem.addEventListener("mouseup", (resetButton) => {
-        newItem.style.backgroundColor = item.style.backgroundColor;
-      });
+    item.style.backgroundColor = "white";
+  });
+  document.querySelectorAll(".quarter-panel").forEach((newItem) => {
+    newItem.addEventListener("mouseup", (resetButton) => {
+      newItem.style.backgroundColor = item.style.backgroundColor;
     });
   });
-}
+});
 
 //function to get player name
 function getPlayerName() {
