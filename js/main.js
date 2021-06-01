437ea1da29e247f252bf75e6a6760fa3f53ac311
Adam Wojdyla <adam@Adams-MacBook-Pro.local>
2021-06-01 12:06:21 -0600

    implementing several ways to automate button flashes

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ let blueBtn = document.querySelector("#blue");
 let startBtn = document.querySelector("#start-button");
 let resetBtn = document.querySelector("#reset-button");
 let buttonArr = [greenBtn, yellowBtn, redBtn, blueBtn];
+computerSequence = ["red", "green", "blue", "yellow"];
 /*----- event listeners -----*/
 
 //document.querySelector("#start-button").addEventListener("click", render);
@@ -29,55 +30,112 @@ document.querySelector("#reset-button").addEventListener("click", init);
 //yellowBtn.addEventListener("click", userGenSequence);
 //blueBtn.addEventListener("click", userGenSequence);
 
-computerSequence = ["red", "green", "blue", "yellow"];
-function blink() {
-  for (i = 0; i < computerSequence.length; i++) {
-    console.log(computerSequence[i]);
-    if (computerSequence[i] == "red") {
-      console.log("red is true");
-      redBtn.style.backgroundColor = "white";
-      setTimeout("setToRed()", 500);
+function showMoves() {
+  var i = 0;
+  var moves = setInterval(function(){
+    playGame(computerSequence[i]);
+    i++;
+    if (i >=computerSequence.length) {
+      clearInterval(moves);
     }
-    else if (computerSequence[i] == "blue") {
-      console.log("blue is true");
-      blueBtn.style.backgroundColor = "white";
-      setTimeout("setToBlue()", 500);
-    }
-    else if (computerSequence[i] == "green") {
-      console.log("green is true");
-      blueBtn.style.backgroundColor = "white";
-      setTimeout("setToGreen()", 500);
-    }
-    else if (computerSequence[i] == "yellow") {
-      console.log("yellow is true");
-      blueBtn.style.backgroundColor = "white";
-      setTimeout("setToYellow()", 500);
-    }
-  }
-}
-function setToGreen() {
-  greenBtn.style.backgroundColor = "green";
-}
-function setToRed() {
-  redBtn.style.backgroundColor = "red";
-}
-function setToBlue() {
-  blueBtn.style.backgroundColor = "blue";
-}
-function setToYellow() {
-  yellowBtn.style.backgroundColor = "yellow";
+  }, 600)
+  
 }
 
-//startBlinking();
-//blink();
-
-function startBlinking() {
-  setInterval(function () {
-    console.log("interval");
-    blink();
-    console.log("blink()");
+function playGame(field) {
+  document.getElementById(field).style.opacity="0.3";
+  setTimeout(function(){
+      document.getElementById(field).style.opacity="1";
   }, 1000);
 }
+showMoves();
+
+
+
+
+
+var flashInterval = setInterval(blink(), 1000);
+
+// function setColor() {
+  
+// computerSequence.forEach((color) =>{
+//   if (color === "red") {
+//     console.log("red is true");
+//     redBtn.style.backgroundColor = redBtn.style.backgroundColor == "red" ? "white" : "red";
+//   }
+//   if (color === "blue") {
+//     console.log("blue is true");
+//     blueBtn.style.backgroundColor = blueBtn.style.backgroundColor == "blue" ? "white" : "blue";
+//   }
+//    if (color === "green") {
+//     console.log("green is true");
+//     greenBtn.style.backgroundColor = greenBtn.style.backgroundColor == "green" ? "white" : "green";
+//   }
+//    if (color === "yellow") {
+//     console.log("yellow is true");
+//     yellowBtn.style.backgroundColor = yellowBtn.style.backgroundColor == "yellow" ? "white" : "yellow";
+//   }
+// })
+// }
+
+function stopColor() {
+  clearInterval(flashInterval);
+}
+//stopColor();
+
+ 
+// function blink() {
+  
+  
+//   for (i = 0; i < computerSequence.length; i++) {
+    
+//     if (computerSequence[i] === "red") {
+//       console.log("red is true");
+//       redBtn.style.backgroundColor = "white";
+//       setTimeout("setToRed()", 500);
+//     }
+//      if (computerSequence[i] === "blue") {
+//       console.log("blue is true");
+//       blueBtn.style.backgroundColor = "white";
+//       setTimeout("setToBlue()", 100);
+//     }
+//      if (computerSequence[i] === "green") {
+//       console.log("green is true");
+//       greenBtn.style.backgroundColor = "white";
+//       setTimeout("setToGreen()", 100);
+//     }
+//      if (computerSequence[i] === "yellow") {
+//       console.log("yellow is true");
+//       yellowBtn.style.backgroundColor = "white";
+//       setTimeout("setToYellow()", 1000);
+//     }
+//   }
+  
+// }
+
+// function setToGreen() {
+//   greenBtn.style.backgroundColor = "green";
+// }
+// function setToRed() {
+//   redBtn.style.backgroundColor = "red";
+// }
+// function setToBlue() {
+//   blueBtn.style.backgroundColor = "blue";
+// }
+// function setToYellow() {
+//   yellowBtn.style.backgroundColor = "yellow";
+// }
+
+// //startBlinking();
+// //blink();
+
+// function startBlinking() {
+//   setInterval(function () {
+//     console.log("interval");
+//     blink();
+//     console.log("blink()");
+//   }, 1000);
+// }
 
 
 init();
@@ -108,7 +166,7 @@ function compGenSequence() {
   return computerSequence;
 }
 
-
+// click event that flashes the button
 // document.querySelectorAll(".quarter-panel").forEach((item) => {
 //   item.addEventListener("mousedown", (flashButton) => {
 //     userSequence.push(item.id);
