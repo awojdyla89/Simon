203d8586393254c0d2ee121f424fbf54bd5fff64
Adam Wojdyla <adam@Adams-MacBook-Pro.local>
2021-06-01 16:33:54 -0600

    created a comparison function to check if results of user and computer input

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let playerName;
 let highScore;
 let currentScore;
 let userSequence = [];
-let computerSequence = [];
+
 
 /*----- cached element references -----*/
 
@@ -19,11 +19,11 @@ let redBtn = document.querySelector("#red");
 let blueBtn = document.querySelector("#blue");
 let startBtn = document.querySelector("#start-button");
 let resetBtn = document.querySelector("#reset-button");
-//let computerSequence = [greenBtn, yellowBtn, redBtn, blueBtn];
+let compSequence = []; //[greenBtn, yellowBtn, redBtn, blueBtn];
 //let computerSequence = ["red", "green", "blue", "yellow"];
 /*----- event listeners -----*/
 
-//document.querySelector("#start-button").addEventListener("click", render);
+document.querySelector("#start-button").addEventListener("click", render);
 document.querySelector("#reset-button").addEventListener("click", init);
 //greenBtn.addEventListener("click", blink);
 //redBtn.addEventListener("click", userGenSequence);
@@ -32,109 +32,104 @@ document.querySelector("#reset-button").addEventListener("click", init);
 
 function showMoves() {
   var i = 0;
-  var moves = setInterval(function(){
-    console.log(computerSequence[i])
-    playGame(computerSequence[i]);
+  var moves = setInterval(function () {
+    //console.log("random = " +buttonArr[i].id);
+    //console.log("user = " +userSequence[i]);
+    playGame(compSequence[i]);
     i++;
-    if (i >=buttonArr.length) {
+    if (i >= compSequence.length) {
       clearInterval(moves);
     }
-  }, 600)
-  
+  }, 600);
 }
 
-function playGame(computerSequence) {
-  computerSequence.style.opacity="0.3";
-  setTimeout(function(){
-      computerSequence.style.opacity="1";
-  }, 1000);
-  // computerSequence.forEach(color =>{
-  //   if(color === "red"){
-  //     redBtn.style.opacity="0.3";
-  //     setTimeout(function(){
-  //       redBtn.style.opacity="1";
-  //   }, 1000);
-  //   }
-  //   if(color === "blue"){
-  //     blueBtn.style.opacity="0.3";
-  //     setTimeout(function(){
-  //       blueBtn.style.opacity="1";
-  //   }, 1000);
-  //   }
-  //   if(color === "green"){
-  //     greenBtn.style.opacity="0.3";
-  //     setTimeout(function(){
-  //       greenBtn.style.opacity="1";
-  //   }, 1000);
-  //   }
-  //   if(color === "yellow"){
-  //     yellowBtn.style.opacity="0.3";
-  //     setTimeout(function(){
-  //       yellowBtn.style.opacity="1";
-  //   }, 1000);
-  //   }
-  // })
+function playGame(doc) {
+  doc.style.opacity = "0.6";
+  setTimeout(function () {
+    doc.style.opacity = "1";
+  }, 500);
+}
+
+//init();
+/*----- functions -----*/
+
+// create an init() function that creates the starting foundation of the game
+function init() {
+  
+  playerName.innerText = "";
+  cScore.innerText = 0;
+  hScore.innerText = 0;
+  userSequence = [];
+  compSequence = [];
+
+  //render();
+}
+
+function render(){
+  
+  compGenSequence();
+  showMoves();
+  //compareResults();
   
 }
-//showMoves();
-
-// function convertQuery(){
-//   computerSequence.forEach(color =>{
-//     if(color === "red"){
-//       color = redBtn;
-//     }
-//     if(color === "blue"){
-//       color = blueBtn;
-//     }
-//     if(color === "green"){
-//       color = greenBtn;
-//     }
-//     if(color === "yellow"){
-//       color = yellowBtn;
-//     }
-//   })
-//   return color;
-// }
-
-
-
-
-
-//var flashInterval = setInterval(blink(), 1000);
-
-// function setColor() {
-  
-// computerSequence.forEach((color) =>{
-//   if (color === "red") {
-//     console.log("red is true");
-//     redBtn.style.backgroundColor = redBtn.style.backgroundColor == "red" ? "white" : "red";
-//   }
-//   if (color === "blue") {
-//     console.log("blue is true");
-//     blueBtn.style.backgroundColor = blueBtn.style.backgroundColor == "blue" ? "white" : "blue";
-//   }
-//    if (color === "green") {
-//     console.log("green is true");
-//     greenBtn.style.backgroundColor = greenBtn.style.backgroundColor == "green" ? "white" : "green";
-//   }
-//    if (color === "yellow") {
-//     console.log("yellow is true");
-//     yellowBtn.style.backgroundColor = yellowBtn.style.backgroundColor == "yellow" ? "white" : "yellow";
-//   }
-// })
-// }
-
-function stopColor() {
-  clearInterval(flashInterval);
+let j = 0;
+function compareResults(){
+  if(userSequence.length === compSequence.length){
+    cScore.innerText = j + 1;
+    //console.log(userSequence.length + " == " + compSequence.length)
+    for(let i = 0; i < compSequence.length; i++){
+      if(userSequence[i] == compSequence[i].id){
+        //console.log(userSequence[i] + " == " + compSequence[i].id)
+      }else {
+        console.log(userSequence + " ==> " + compSequence[i].id)
+        alert('YOU LOSE')
+        return;
+      }
+    }
+    j++;
+    userSequence =[];
+    render();
+  }
 }
-//stopColor();
 
- 
-// function blink() {
-  
-  
+// create a randomNum function that generates a random number between 1-4
+function compGenSequence() {
+  let choices = [greenBtn, redBtn, blueBtn, yellowBtn];
+  let randomIndex = Math.floor(Math.random() * choices.length - 1) + 1;
+  compSequence.push(choices[randomIndex]);
+
+  return compSequence;
+}
+
+//click event that flashes the button
+document.querySelectorAll(".quarter-panel").forEach((item) => {
+  item.addEventListener("mousedown", (flashButton) => {
+    userSequence.push(item.id);
+    compareResults();
+    //compGenSequence();
+    //showMoves();
+    //item.style.backgroundColor = "white";
+    item.style.opacity = "0.8";
+  });
+  document.querySelectorAll(".quarter-panel").forEach((newItem) => {
+    newItem.addEventListener("mouseup", (resetButton) => {
+      //newItem.style.backgroundColor = item.style.backgroundColor;
+      newItem.style.opacity = "1.0";
+    });
+  });
+});
+
+//function to get player name
+function getPlayerName() {
+  let namePrompt = prompt("Please enter your name");
+  if (namePrompt != null) {
+    playerName.innerText = namePrompt;
+  }
+  return playerName;
+}
+
 //   for (i = 0; i < computerSequence.length; i++) {
-    
+
 //     if (computerSequence[i] === "red") {
 //       console.log("red is true");
 //       redBtn.style.backgroundColor = "white";
@@ -156,7 +151,7 @@ function stopColor() {
 //       setTimeout("setToYellow()", 1000);
 //     }
 //   }
-  
+
 // }
 
 // function setToGreen() {
@@ -171,67 +166,37 @@ function stopColor() {
 // function setToYellow() {
 //   yellowBtn.style.backgroundColor = "yellow";
 // }
-
-// //startBlinking();
-// //blink();
-
-// function startBlinking() {
-//   setInterval(function () {
-//     console.log("interval");
-//     blink();
-//     console.log("blink()");
+// function playGame(field) {
+//   document.getElementById(field).style.opacity="0.3";
+//   setTimeout(function(){
+//       document.getElementById(field).style.opacity="1";
 //   }, 1000);
 // }
+//showMoves();
 
+//var flashInterval = setInterval(blink(), 1000);
 
-init();
-/*----- functions -----*/
+// function setColor() {
 
-// create an init() function that creates the starting foundation of the game
-function init() {
-  playerName.innerText = "";
-  cScore.innerText = 0;
-  hScore.innerText = 0;
-  userSequence = [];
-  computerSequence = [];
-
-  //render();
-}
-
-// function render() {
-//     getPlayerName();
-
+// computerSequence.forEach((color) =>{
+//   if (color === "red") {
+//     console.log("red is true");
+//     redBtn.style.backgroundColor = redBtn.style.backgroundColor == "red" ? "white" : "red";
+//   }
+//   if (color === "blue") {
+//     console.log("blue is true");
+//     blueBtn.style.backgroundColor = blueBtn.style.backgroundColor == "blue" ? "white" : "blue";
+//   }
+//    if (color === "green") {
+//     console.log("green is true");
+//     greenBtn.style.backgroundColor = greenBtn.style.backgroundColor == "green" ? "white" : "green";
+//   }
+//    if (color === "yellow") {
+//     console.log("yellow is true");
+//     yellowBtn.style.backgroundColor = yellowBtn.style.backgroundColor == "yellow" ? "white" : "yellow";
+//   }
+// })
 // }
-
-// create a randomNum function that generates a random number between 1-4
-function compGenSequence() {
-  let choices = [greenBtn, redBtn, blueBtn, yellowBtn];
-  let randomIndex = Math.floor(Math.random() * choices.length - 1) + 1;
-  computerSequence.push(choices[randomIndex]);
-
-  return computerSequence;
-}
-
-//click event that flashes the button
-document.querySelectorAll(".quarter-panel").forEach((item) => {
-  item.addEventListener("mousedown", (flashButton) => {
-    userSequence.push(item.id);
-    compGenSequence();
-    //showMoves();
-    item.style.backgroundColor = "white";
-  });
-  document.querySelectorAll(".quarter-panel").forEach((newItem) => {
-    newItem.addEventListener("mouseup", (resetButton) => {
-      newItem.style.backgroundColor = item.style.backgroundColor;
-    });
-  });
-});
-
-//function to get player name
-function getPlayerName() {
-  let namePrompt = prompt("Please enter your name");
-  if (namePrompt != null) {
-    playerName.innerText = namePrompt;
-  }
-  return playerName;
-}
+// function stopColor() {
+//   clearInterval(flashInterval);
+// }
